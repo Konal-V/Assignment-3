c2163870fe7a605e51676b90c3a33975d5cfa454
Konal-V <konalvasisth0723@gmail.com>
2025-04-21 01:59:43 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,12 @@
+# Factorial using recursion
+'''
+def factorial(n):
+    if n < 2:
+        return 1
+    else:
+        return n * (factorial(n-1))
+result = int(input("Enter a number:"))
+print(f"The factorial of {result} is:{factorial(result)}")
+'''
+
+    
---
+++ Task 2.py
@@ -0,0 +1,17 @@
+#square root , natural logrithm, sine of a number using module
+'''
+a=int(input("Enter a number:"))
+from math import *
+
+print(f"square root: {sqrt(a)}")
+print(f"Logarithm: {log(a)}")
+print(f"sine: {sin(a)}")
+'''
+'''
+a=int(input("Enter a number:"))
+from math import sqrt,log,sin
+
+print(f"square root: {sqrt(a)}")
+print(f"Logarithm: {log(a)}")
+print(f"sine: {sin(a)}")
+'''
